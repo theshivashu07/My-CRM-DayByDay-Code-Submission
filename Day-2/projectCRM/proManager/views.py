d23b86e701611640a0434586d2ab8bd00173c8aa
Shivam Shukla <shivamshukla3108@gmail.com>
2022-02-16 13:01:43 +0530

    Day-2

---
+++ Day-2/projectCRM/proManager/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+	return render(request,"proManager/index.html");
+ 
+def about(request):
+	return render(request,"proManager/about.html");
+
+def contact(request):
+	return render(request,"proManager/contact.html");
+
+def gym(request):
+	return render(request,"proManager/gym.html");
+
+def price(request):
+	return render(request,"proManager/price.html");
+
+
+'''
+def masters(request):
+	return HttpResponse("Hey Shivashu! You are here, as Project Manager. and this is masters");
+'''
