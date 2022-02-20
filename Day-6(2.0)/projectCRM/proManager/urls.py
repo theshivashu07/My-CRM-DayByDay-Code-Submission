745cba42435e19f43d7a55e06f11ffa070f8c48b
Shivam Shukla <shivamshukla3108@gmail.com>
2022-02-20 23:11:55 +0530

    Day-6(2.0)

---
+++ Day-6(2.0)/projectCRM/proManager/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+		path('',views.index,name='index'),
+		path('home/',views.index,name='index'),
+		path('about/',views.about,name='about'),
+		path('contact/',views.contact,name='contact'),
+		path('gym/',views.gym,name='gym'),
+		path('price/',views.price,name='price'),
+		path('login/',views.login,name='login'),
+		#path('masters',views.masters,name='masters'),
+]
+
