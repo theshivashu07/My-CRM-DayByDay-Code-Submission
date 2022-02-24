e65f943ce0a21afdf1c58c4a704c329059a4e77d
Shivam Shukla <shivamshukla3108@gmail.com>
2022-02-24 17:35:28 +0530

    Day-10

---
+++ Day-10/projectCRM/Guest/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+	return render(request,"Guest/index.html");
+ 
+def about(request):
+	return render(request,"Guest/about.html");
+
+def contact(request):
+	return render(request,"Guest/contact.html");
+
+def gym(request):
+	return render(request,"Guest/gym.html");
+
+def price(request):
+	return render(request,"Guest/price.html");
+
+def login(request):
+	return render(request,"Guest/login2.html");
+
+
+
